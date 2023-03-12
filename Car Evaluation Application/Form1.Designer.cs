b4e7b0070bb76e603495880bc6446bb74252cd4e
citubrentlasaca <111823676+citubrentlasaca@users.noreply.github.com>
2023-03-12 12:36:30 +0800

    Initial Commit

---
+++ Car Evaluation Application/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Car_Evaluation_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.epochtxt = new System.Windows.Forms.TextBox();
+            this.trainbtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pricetxt = new System.Windows.Forms.TextBox();
+            this.mainttxt = new System.Windows.Forms.TextBox();
+            this.doortxt = new System.Windows.Forms.TextBox();
+            this.persontxt = new System.Windows.Forms.TextBox();
+            this.lugtxt = new System.Windows.Forms.TextBox();
+            this.safetytxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Epoch:";
+            // 
+            // epochtxt
+            // 
+            this.epochtxt.Location = new System.Drawing.Point(115, 8);
+            this.epochtxt.Margin = new System.Windows.Forms.Padding(2);
+            this.epochtxt.Multiline = true;
+            this.epochtxt.Name = "epochtxt";
+            this.epochtxt.Size = new System.Drawing.Size(76, 22);
+            this.epochtxt.TabIndex = 1;
+            this.epochtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // trainbtn
+            // 
+            this.trainbtn.Location = new System.Drawing.Point(196, 8);
+            this.trainbtn.Name = "trainbtn";
+            this.trainbtn.Size = new System.Drawing.Size(75, 22);
+            this.trainbtn.TabIndex = 2;
+            this.trainbtn.Text = "Train";
+            this.trainbtn.UseVisualStyleBackColor = true;
+            this.trainbtn.Click += new System.EventHandler(this.trainbtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Price:";
+            // 
+            // pricetxt
+            // 
+            this.pricetxt.Location = new System.Drawing.Point(115, 34);
+            this.pricetxt.Margin = new System.Windows.Forms.Padding(2);
+            this.pricetxt.Multiline = true;
+            this.pricetxt.Name = "pricetxt";
+            this.pricetxt.Size = new System.Drawing.Size(76, 22);
+            this.pricetxt.TabIndex = 4;
+            this.pricetxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mainttxt
+            // 
+            this.mainttxt.Location = new System.Drawing.Point(115, 60);
+            this.mainttxt.Margin = new System.Windows.Forms.Padding(2);
+            this.mainttxt.Multiline = true;
+            this.mainttxt.Name = "mainttxt";
+            this.mainttxt.Size = new System.Drawing.Size(76, 22);
+            this.mainttxt.TabIndex = 5;
+            this.mainttxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // doortxt
+            // 
+            this.doortxt.Location = new System.Drawing.Point(115, 86);
+            this.doortxt.Margin = new System.Windows.Forms.Padding(2);
+            this.doortxt.Multiline = true;
+            this.doortxt.Name = "doortxt";
+            this.doortxt.Size = new System.Drawing.Size(76, 22);
+            this.doortxt.TabIndex = 6;
+            this.doortxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // persontxt
+            // 
+            this.persontxt.Location = new System.Drawing.Point(115, 112);
+            this.persontxt.Margin = new System.Windows.Forms.Padding(2);
+            this.persontxt.Multiline = true;
+            this.persontxt.Name = "persontxt";
+            this.persontxt.Size = new System.Drawing.Size(76, 22);
+            this.persontxt.TabIndex = 7;
+            this.persontxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lugtxt
+            // 
+            this.lugtxt.Location = new System.Drawing.Point(115, 138);
+            this.lugtxt.Margin = new System.Windows.Forms.Padding(2);
+            this.lugtxt.Multiline = true;
+            this.lugtxt.Name = "lugtxt";
+            this.lugtxt.Size = new System.Drawing.Size(76, 22);
+            this.lugtxt.TabIndex = 8;
+            this.lugtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // safetytxt
+            // 
+            this.safetytxt.Location = new System.Drawing.Point(115, 164);
+            this.safetytxt.Margin = new System.Windows.Forms.Padding(2);
+            this.safetytxt.Multiline = true;
+            this.safetytxt.Name = "safetytxt";
+            this.safetytxt.Size = new System.Drawing.Size(76, 22);
+            this.safetytxt.TabIndex = 9;
+            this.safetytxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Maintenance Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 115);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Person Capacity:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 89);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Number of Doors:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 141);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Luggage Boot Size:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(71, 165);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Safety:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(195, 37);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(258, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "1 = Very High | 0.66 = High | 0.33 = Medium | 0 = Low";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(195, 63);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(258, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "1 = Very High | 0.66 = High | 0.33 = Medium | 0 = Low";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(195, 89);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(104, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "0 = 2 | 0.5 = 3 | 1 = 4";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(195, 115);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(121, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "0 = 2 | 0.5 = 4 | 1 = more";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(195, 141);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(163, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "0 = Small | 0.5 = Medium | 1 = Big";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(195, 167);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(165, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "0 = Low | 0.5 = Medium | 1 = High";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(438, 22);
+            this.button1.TabIndex = 21;
+            this.button1.Text = " Evaluate Car";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputtxt
+            // 
+            this.outputtxt.Location = new System.Drawing.Point(15, 218);
+            this.outputtxt.Margin = new System.Windows.Forms.Padding(2);
+            this.outputtxt.Multiline = true;
+            this.outputtxt.Name = "outputtxt";
+            this.outputtxt.ReadOnly = true;
+            this.outputtxt.Size = new System.Drawing.Size(438, 22);
+            this.outputtxt.TabIndex = 22;
+            this.outputtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 252);
+            this.Controls.Add(this.outputtxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.safetytxt);
+            this.Controls.Add(this.lugtxt);
+            this.Controls.Add(this.persontxt);
+            this.Controls.Add(this.doortxt);
+            this.Controls.Add(this.mainttxt);
+            this.Controls.Add(this.pricetxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trainbtn);
+            this.Controls.Add(this.epochtxt);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Car Evaluation Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox epochtxt;
+        private System.Windows.Forms.Button trainbtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pricetxt;
+        private System.Windows.Forms.TextBox mainttxt;
+        private System.Windows.Forms.TextBox doortxt;
+        private System.Windows.Forms.TextBox persontxt;
+        private System.Windows.Forms.TextBox lugtxt;
+        private System.Windows.Forms.TextBox safetytxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox outputtxt;
+    }
+}
+
---
+++ Car Evaluation Application/Form1.cs
@@ -0,0 +1,85 @@
+﻿using Backprop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Car_Evaluation_Application
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet neuralNet;
+        double[,] data = new double[,] {
+            {1, 1, 0, 0, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0.5, 0},
+            {1, 1, 0, 0, 0, 1, 0},
+            {1, 1, 0, 0, 0.5, 0, 0},
+            {1, 1, 0, 0, 0.5, 0.5, 0},
+            {1, 1, 0, 0, 0.5, 1, 0},
+            {1, 1, 0, 0, 1, 0, 0},
+            {1, 1, 0, 0, 1, 0.5, 0},
+            {1, 0.66, 0, 0.5, 0, 1, 0.33},
+            {1, 0.66, 0, 0.5, 0.5, 1, 0.33},
+            {1, 0.66, 0, 0.5, 1, 0.5, 0.33},
+            {1, 0.66, 0, 0.5, 1, 1, 0.33},
+            {1, 0.66, 0, 1, 0.5, 1, 0.33},
+            {1, 0.66, 0, 1, 1, 0.5, 0.33},
+            {1, 0.66, 0, 1, 1, 1, 0.33},
+            {1, 0.66, 0.5, 0.5, 0, 1, 0.33},
+            {0.33, 0, 0, 0.5, 1, 1, 0.66},
+            {0.33, 0, 0, 0.5, 0.5, 1, 0.66},
+            {0.33, 0, 0, 0.5, 1, 0.5, 0.66},
+            {0.33, 0, 0, 1, 0.5, 1, 0.66},
+            {0.33, 0, 0, 1, 1, 0.5, 0.66},
+            {0.33, 0, 0.5, 4, 0, 1, 0.66},
+            {0.33, 0, 0.5, 4, 0.5, 1, 0.66},
+            {0.33, 0, 0.5, 1, 0, 1, 0.66},
+            {0.33, 0, 0.5, 1, 0.5, 1, 1},
+            {0.33, 0, 0.5, 1, 1, 1, 1},
+            {0.33, 0, 1, 4, 0.5, 1, 1},
+            {0.33, 0, 1, 4, 1, 1, 1},
+            {0.33, 0, 1, 1, 0.5, 1, 1},
+            {0.33, 0, 1, 1, 1, 1, 1},
+            {0.33, 0, 1, 4, 0.5, 1, 1},
+            {0.33, 0, 1, 1, 1, 1, 1}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            neuralNet = new NeuralNet(6, 10, 1);
+        }
+
+        private void trainbtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                neuralNet.setInputs(0, data[i, 0]);
+                neuralNet.setInputs(1, data[i, 1]);
+                neuralNet.setInputs(2, data[i, 2]);
+                neuralNet.setInputs(3, data[i, 3]);
+                neuralNet.setInputs(4, data[i, 4]);
+                neuralNet.setInputs(5, data[i, 5]);
+                neuralNet.setDesiredOutput(0, data[i, 6]);
+                neuralNet.learn();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            neuralNet.setInputs(0, Convert.ToDouble(pricetxt.Text));
+            neuralNet.setInputs(1, Convert.ToDouble(mainttxt.Text));
+            neuralNet.setInputs(2, Convert.ToDouble(doortxt.Text));
+            neuralNet.setInputs(3, Convert.ToDouble(persontxt.Text));
+            neuralNet.setInputs(4, Convert.ToDouble(lugtxt.Text));
+            neuralNet.setInputs(5, Convert.ToDouble(safetytxt.Text));
+            neuralNet.run();
+            outputtxt.Text = "" + neuralNet.getOuputData(0);
+        }
+    }
+}
